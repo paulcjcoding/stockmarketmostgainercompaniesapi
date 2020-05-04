0530dbd31ad113570a722b44d6124d0a0c9c1c54
paulcjohnson25 <36369309+paulcjohnson25@users.noreply.github.com>
2020-05-04 17:51:51 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,85 @@
+var symbolName = 'Facebook Inc.';
+var symbol = 'FB';
+document.addEventListener('DOMContentLoaded', () => {
+    // https://financialmodelingprep.com/developer/docs
+    getRequest(
+        'https://financialmodelingprep.com/api/v3/stock/gainers',
+        drawOutput
+    );
+
+    function drawOutput(responseText) {
+
+        let resp = Object.values(JSON.parse(responseText).mostGainerStock);
+
+
+        let $table = document.createElement("table");
+        $table.className += " table";
+
+        var elements = document.querySelectorAll('.stock-name')[0];
+
+        let $head = document.createElement("thead");
+        let $body = document.createElement("tbody");
+
+        let $lineHader = document.createElement("tr");
+
+        for (let i = 0; i < resp.length; i++) {
+            let financial = resp[i];
+            let $line = document.createElement("tr");
+
+
+            for (var key in financial) {
+                if (i === 0 && financial.hasOwnProperty(key)) {
+                    let $ele = document.createElement("th");
+                    $ele.textContent = key;
+                    $lineHader.appendChild($ele);
+                }
+            }
+
+            $head.appendChild($lineHader);
+            $table.appendChild($head);
+
+            for (var key2 in financial) {
+                if (financial.hasOwnProperty(key2)) {
+                    let $eletd = document.createElement("td");
+                    $eletd.textContent = financial[key2];
+                    $line.appendChild($eletd);
+                }
+            }
+            $body.appendChild($line)
+
+            $table.appendChild($body);
+
+        }
+
+        document.body.appendChild($table);
+    }
+
+    function getRequest(url, success) {
+        var req = false;
+        try {
+            req = new XMLHttpRequest();
+        } catch (e) {
+            try {
+                req = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    req = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {
+                    return false;
+                }
+            }
+        }
+        if (!req) return false;
+        if (typeof success != 'function') success = function() {};
+        req.onreadystatechange = function() {
+            if (req.readyState == 4) {
+                if (req.status === 200) {
+                    success(req.responseText)
+                }
+            }
+        }
+        req.open("GET", url, true);
+        req.send(null);
+        return req;
+    }
+})
